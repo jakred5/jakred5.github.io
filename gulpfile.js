658b6e7848379288187bd8b9861abc2fbd327c79
Jacob Keifer <jacobkeifer@MacBook-Pro-6.local>
2015-12-04 22:20:48 -0800

    added minify html

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,27 @@ var gulp = require('gulp'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
     minifyCss = require('gulp-minify-css'),
+    minifyHTML = require('gulp-minify-html'),
     runSequence = require('run-sequence');
 
 gulp.task('default', function() {
-    runSequence('index', 'rest');
+    runSequence('index', 'rest', 'minifyIndexExternals', 'minifyRestExternals');
+});
+
+gulp.task('minifyIndexExternals', function() {
+    return gulp.src('index.html')
+        .pipe(useref())
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(gulp.dest(''));
+});
+
+gulp.task('minifyRestExternals', function() {
+    return gulp.src('views/*.html')
+        .pipe(useref())
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(gulp.dest('views'));
 });
 
 gulp.task('index', function() {
